84cfd769bfae482ec0bc7a409eed233266aa004a
muhammedalaa1 <muhammedalaa2213@gmail.com>
2023-07-11 23:14:00 +0300

    t

--- src/Components/Checkout/Reciept.jsx
+++ src/Components/Checkout/Reciept.jsx
@@ -29,7 +29,7 @@ const Reciept = ({ info }) => {
   };
   useEffect(() => {
     if (cartItems.length === 0) {
-      // navigate("/", { replace: true });
+      navigate("/", { replace: true });
     }
     if (cartItems.length > 0) {
       addToOrders(cartItems);
